afaa7d9459a12bc04a68ecea301936940f4b8a38
Marcus Asteborg <xnorpx@outlook.com>
2024-12-25 11:14:05 -0800

    Check for updates

--- src/api.rs
+++ src/api.rs
@@ -1,5 +1,7 @@
+use anyhow::{anyhow, Context};
 use axum::body::Bytes;
 use serde::{Deserialize, Serialize};
+use std::cmp::Ordering;
 use std::fmt::Debug;
 
 #[derive(Default)]
@@ -98,4 +100,120 @@ pub struct VersionInfo {
     pub build: u32,
     pub file: String,
     pub releaseNotes: String,
+}
+
+impl VersionInfo {
+    pub fn parse(version_str: &str, release_notes: Option<String>) -> anyhow::Result<Self> {
+        let parts: Vec<_> = version_str.trim().split('.').collect();
+        let major: u8 = parts
+            .first()
+            .ok_or_else(|| anyhow!("Missing major version segment"))?
+            .parse()
+            .context("Failed to parse major version")?;
+        let minor: u8 = parts
+            .get(1)
+            .ok_or_else(|| anyhow!("Missing minor version segment"))?
+            .parse()
+            .context("Failed to parse minor version")?;
+        let patch: u8 = parts
+            .get(2)
+            .ok_or_else(|| anyhow!("Missing patch version segment"))?
+            .parse()
+            .context("Failed to parse patch version")?;
+
+        Ok(Self {
+            major,
+            minor,
+            patch,
+            releaseNotes: release_notes.unwrap_or_default(),
+            ..Default::default()
+        })
+    }
+}
+impl PartialEq for VersionInfo {
+    fn eq(&self, other: &Self) -> bool {
+        self.major == other.major && self.minor == other.minor && self.patch == other.patch
+    }
+}
+
+impl Eq for VersionInfo {}
+
+impl PartialOrd for VersionInfo {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for VersionInfo {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match self.major.cmp(&other.major) {
+            Ordering::Equal => match self.minor.cmp(&other.minor) {
+                Ordering::Equal => self.patch.cmp(&other.patch),
+                other_order => other_order,
+            },
+            other_order => other_order,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::VersionInfo;
+    use std::cmp::Ordering;
+
+    #[test]
+    fn test_eq_and_ne() {
+        let v1 = VersionInfo {
+            major: 1,
+            minor: 2,
+            patch: 3,
+            ..Default::default()
+        };
+        let v2 = VersionInfo {
+            major: 1,
+            minor: 2,
+            patch: 3,
+            ..Default::default()
+        };
+        let v3 = VersionInfo {
+            major: 1,
+            minor: 3,
+            patch: 3,
+            ..Default::default()
+        };
+        assert_eq!(v1, v2);
+        assert_ne!(v1, v3);
+        assert!(v3 > v2);
+        assert!(v2 <= v1);
+    }
+
+    #[test]
+    fn test_partial_ord_and_ord() {
+        let v1 = VersionInfo {
+            major: 1,
+            minor: 2,
+            patch: 3,
+            ..Default::default()
+        };
+        let v2 = VersionInfo {
+            major: 1,
+            minor: 2,
+            patch: 4,
+            ..Default::default()
+        };
+        let v3 = VersionInfo {
+            major: 2,
+            minor: 0,
+            patch: 0,
+            ..Default::default()
+        };
+
+        // Check ordering with v1 and v2
+        assert_eq!(v1.cmp(&v2), Ordering::Less);
+        assert!(v1 < v2);
+
+        // Check ordering with v3 and v2
+        assert_eq!(v3.cmp(&v2), Ordering::Greater);
+        assert!(v3 > v2);
+    }
 }
