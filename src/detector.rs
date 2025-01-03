56e7d40ffaf728fd94d96232958018a27779d985
Marcus Asteborg <xnorpx@outlook.com>
2025-01-02 17:50:52 -0800

    Add support for Custom Yolo5 models (#72)

--- src/detector.rs
+++ src/detector.rs
@@ -6,9 +6,9 @@ use crate::{
         Resizer,
     },
 };
-use anyhow::bail;
+use anyhow::{anyhow, bail};
 use bytes::Bytes;
-use ndarray::{Array, ArrayView, Axis};
+use ndarray::{s, Array, ArrayView, Axis};
 use ort::{
     execution_providers::DirectMLExecutionProvider,
     inputs,
@@ -79,12 +79,14 @@ pub struct OnnxConfig {
 pub enum ObjectDetectionModel {
     #[default]
     RtDetrv2,
+    Yolo5,
 }
 
 impl std::fmt::Display for ObjectDetectionModel {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             ObjectDetectionModel::RtDetrv2 => write!(f, "rt-detrv2"),
+            ObjectDetectionModel::Yolo5 => write!(f, "yolo5"),
         }
     }
 }
@@ -92,14 +94,15 @@ impl std::fmt::Display for ObjectDetectionModel {
 impl ObjectDetectionModel {
     pub fn pre_process<'a>(
         &self,
-        resized_image: &'a Image,
         input: &'a mut Array<f32, ndarray::Dim<[usize; 4]>>,
         orig_size: &'a Array<i64, ndarray::Dim<[usize; 2]>>,
     ) -> SessionInputs<'a, 'a> {
         match self {
-            Self::RtDetrv2 => rt_detrv2_pre_process(resized_image, input, orig_size),
+            Self::RtDetrv2 => rt_detrv2_pre_process(input, orig_size),
+            Self::Yolo5 => yolo5_pre_process(input),
         }
     }
+
     pub fn post_process(
         &self,
         outputs: SessionOutputs<'_, '_>,
@@ -118,27 +121,31 @@ impl ObjectDetectionModel {
                 object_filter,
                 object_classes,
             ),
+
+            Self::Yolo5 => yolo5_post_process(
+                outputs,
+                confidence_threshold,
+                resize_factor_x,
+                resize_factor_y,
+                object_filter,
+                object_classes,
+            ),
         }
     }
 }
 
 fn rt_detrv2_pre_process<'a>(
-    resized_image: &'a Image,
     input: &'a mut Array<f32, ndarray::Dim<[usize; 4]>>,
+
     orig_size: &'a Array<i64, ndarray::Dim<[usize; 2]>>,
 ) -> SessionInputs<'a, 'a> {
-    for (i, pixel) in resized_image.pixels.iter().enumerate() {
-        let channel = i % 3;
-        let value = *pixel as f32 / 255.0;
-        let index = i / 3;
-        let y = index / 640;
-        let x = index % 640;
-        input[[0, channel, y, x]] = value;
-    }
-
     ort::session::SessionInputs::ValueMap(
         inputs!["images" => input.view(), "orig_target_sizes" => orig_size.view()].unwrap(),
     )
+}
+
+fn yolo5_pre_process(input: &mut Array<f32, ndarray::Dim<[usize; 4]>>) -> SessionInputs {
+    ort::session::SessionInputs::ValueMap(inputs!["images" => input.view()].unwrap())
 }
 
 fn rt_detrv2_post_process(
@@ -156,6 +163,7 @@ fn rt_detrv2_post_process(
     let bboxes = bboxes.index_axis(Axis(0), 0);
     let scores = scores.index_axis(Axis(0), 0);
     let mut predictions = SmallVec::<[Prediction; 10]>::new();
+
     for (i, bbox) in bboxes.outer_iter().enumerate() {
         if scores[i] > confidence_threshold {
             // If object filter is set, skip objects that are not in the filter
@@ -165,6 +173,7 @@ fn rt_detrv2_post_process(
                     continue;
                 }
             }
+
             let prediction = Prediction {
                 x_min: (bbox[0] * resize_factor_x) as usize,
                 x_max: (bbox[2] * resize_factor_x) as usize,
@@ -173,12 +182,136 @@ fn rt_detrv2_post_process(
                 confidence: scores[i],
                 label: object_classes[labels[i] as usize].clone(),
             };
+
             debug!("Prediction - {}: {:?}", predictions.len() + 1, prediction);
+
             predictions.push(prediction);
         }
     }
 
     Ok(predictions)
+}
+
+fn yolo5_post_process(
+    outputs: SessionOutputs<'_, '_>,
+    confidence_threshold: f32,
+    resize_factor_x: f32,
+    resize_factor_y: f32,
+    object_filter: &Option<Vec<bool>>,
+    object_classes: &[String],
+) -> anyhow::Result<SmallVec<[Prediction; 10]>> {
+    let output = outputs.values().next().ok_or(anyhow!("No outputs"))?;
+    let yolo_output: ArrayView<f32, _> = output.try_extract_tensor::<f32>()?.squeeze();
+    assert_eq!(
+        yolo_output.shape()[1],
+        5 + object_classes.len(),
+        "Unexpected yolo_output shape, expected {}, got {}. This probably means that your classes YAML file does not match the model.",
+        5 + object_classes.len(),
+        yolo_output.shape()[1]
+    );
+
+    let mut predictions = SmallVec::<[Prediction; 10]>::new();
+
+    for iter in yolo_output.outer_iter() {
+        if iter[4] > confidence_threshold {
+            let class_idx = iter
+                .slice(s![5..])
+                .iter()
+                .enumerate()
+                .max_by(|a, b| a.1.partial_cmp(b.1).unwrap())
+                .map(|(idx, _)| idx)
+                .unwrap_or(0);
+
+            if let Some(object_filter) = object_filter {
+                if !object_filter[class_idx] {
+                    continue;
+                }
+            }
+
+            let x_center = iter[0] * resize_factor_x;
+            let y_center = iter[1] * resize_factor_y;
+            let width = iter[2] * resize_factor_x;
+            let height = iter[3] * resize_factor_y;
+            let prediction = Prediction {
+                x_min: (x_center - width / 2.0) as usize,
+                y_min: (y_center - height / 2.0) as usize,
+                x_max: (x_center + width / 2.0) as usize,
+                y_max: (y_center + height / 2.0) as usize,
+                confidence: iter[4],
+                label: object_classes[class_idx].clone(),
+            };
+            predictions.push(prediction);
+        }
+    }
+
+    // Apply non-maximum suppression (aka remove overlapping boxes)
+    let predictions = non_maximum_suppression(predictions, 0.5)?;
+
+    for (i, prediction) in predictions.iter().enumerate() {
+        debug!("Prediction - {}: {:?}", i + 1, prediction);
+    }
+
+    Ok(predictions)
+}
+
+fn non_maximum_suppression(
+    mut predictions: SmallVec<[Prediction; 10]>,
+    iou_threshold: f32,
+) -> anyhow::Result<SmallVec<[Prediction; 10]>> {
+    let mut filtered_predictions = SmallVec::new();
+
+    predictions.sort_by(|a, b| {
+        a.label
+            .cmp(&b.label)
+            .then(b.confidence.partial_cmp(&a.confidence).unwrap())
+    });
+
+    let mut current_class = None;
+    let mut kept: SmallVec<[Prediction; 10]> = SmallVec::new();
+
+    for pred in predictions.iter() {
+        if Some(&pred.label) != current_class {
+            for kept_pred in kept.iter() {
+                filtered_predictions.push(kept_pred.clone());
+            }
+            kept.clear();
+            current_class = Some(&pred.label);
+        }
+        let mut should_keep = true;
+        for kept_pred in kept.iter() {
+            if calculate_iou(pred, kept_pred) >= iou_threshold {
+                should_keep = false;
+                break;
+            }
+        }
+
+        if should_keep {
+            kept.push(pred.clone());
+        }
+    }
+
+    for kept_pred in kept.iter() {
+        filtered_predictions.push(kept_pred.clone());
+    }
+
+    Ok(filtered_predictions)
+}
+
+fn calculate_iou(a: &Prediction, b: &Prediction) -> f32 {
+    let x_min = a.x_min.max(b.x_min) as f32;
+    let y_min = a.y_min.max(b.y_min) as f32;
+    let x_max = a.x_max.min(b.x_max) as f32;
+    let y_max = a.y_max.min(b.y_max) as f32;
+    let intersection = (x_max - x_min).max(0.0) * (y_max - y_min).max(0.0);
+    let area_a = (a.x_max - a.x_min) as f32 * (a.y_max - a.y_min) as f32;
+    let area_b = (b.x_max - b.x_min) as f32 * (b.y_max - b.y_min) as f32;
+    let union = area_a + area_b - intersection;
+
+    if union == 0.0 {
+        0.0
+    } else {
+        intersection / union
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -196,7 +329,6 @@ pub struct DetectorConfig {
 impl Detector {
     pub fn new(detector_config: DetectorConfig) -> anyhow::Result<Self> {
         let object_classes = get_object_classes(detector_config.object_classes)?;
-
         let mut object_filter = None;
         if !detector_config.object_filter.is_empty() {
             let mut object_filter_vector = vec![false; object_classes.len()];
@@ -239,6 +371,7 @@ impl Detector {
             "Detector warmed up in: {:?}",
             detector_warmup_start_time.elapsed()
         );
+
         Ok(detector)
     }
 
@@ -268,10 +401,12 @@ impl Detector {
         let processing_time_start = Instant::now();
         decode_jpeg(image_name.clone(), image_bytes, &mut self.decoded_image)?;
         let decode_image_time = processing_time_start.elapsed();
+
         debug!(
             "Decode image time: {:?}, resolution {}x{}",
             decode_image_time, self.decoded_image.width, self.decoded_image.height
         );
+
         let resize_factor_x = self.decoded_image.width as f32 / 640.0;
         let resize_factor_y = self.decoded_image.height as f32 / 640.0;
         let orig_size = Array::from_shape_vec(
@@ -281,29 +416,42 @@ impl Detector {
                 self.resized_image.height as i64,
             ],
         )?;
-
         let resize_image_start_time = Instant::now();
         self.resizer
             .resize_image(&mut self.decoded_image, &mut self.resized_image)?;
         let resize_image_time = resize_image_start_time.elapsed();
         debug!("Resize image time: {:#?}", resize_image_time);
+        let copy_pixels_to_input_start = Instant::now();
+        for (index, chunk) in self.resized_image.pixels.chunks_exact(3).enumerate() {
+            let y = index / 640;
+            let x = index % 640;
+            self.input[[0, 0, y, x]] = chunk[0] as f32 / 255.0;
+            self.input[[0, 1, y, x]] = chunk[1] as f32 / 255.0;
+            self.input[[0, 2, y, x]] = chunk[2] as f32 / 255.0;
+        }
 
-        let session_inputs = self.object_detection_model.pre_process(
-            &self.resized_image,
-            &mut self.input,
-            &orig_size,
+        debug!(
+            "Copy pixels to input time: {:?}",
+            copy_pixels_to_input_start.elapsed()
+        );
+
+        let pre_process_model_input_start = Instant::now();
+        let session_inputs = self
+            .object_detection_model
+            .pre_process(&mut self.input, &orig_size);
+
+        debug!(
+            "Pre-process model input time: {:?}",
+            pre_process_model_input_start.elapsed()
         );
 
         let pre_processing_time = processing_time_start.elapsed();
         debug!("Pre-process time: {:?}", pre_processing_time);
-
         let start_inference_time = std::time::Instant::now();
         let outputs: SessionOutputs = self.session.run(session_inputs)?;
         let inference_time = start_inference_time.elapsed();
         debug!("Inference time: {:?}", inference_time);
-
         let post_processing_time_start = Instant::now();
-
         let confidence_threshold = min_confidence.unwrap_or(self.confidence_threshold);
         let predictions = self.object_detection_model.post_process(
             outputs,
@@ -318,12 +466,13 @@ impl Detector {
         let post_processing_time = now.duration_since(post_processing_time_start);
         debug!("Post-processing time: {:?}", post_processing_time);
         let processing_time = now.duration_since(processing_time_start);
+
         // Processing time is mainly composed of:
         //  1. Image decoding time
         //  2. Image resizing time
         //  3. Inference time
-        debug!("Processing time: {:?}", processing_time);
 
+        debug!("Processing time: {:?}", processing_time);
         if !predictions.is_empty() && image_name.is_some() {
             debug!(
                 "Detected {} objects in image {:?}",
@@ -366,7 +515,6 @@ impl Detector {
         const TUNE_RUNS: usize = 10;
         info!("Running detector {TUNE_RUNS} times to get min processing time");
         let mut min_processing_time = Duration::MAX;
-
         for _ in 0..TUNE_RUNS {
             let detector_warmup_start_time = Instant::now();
             self.detect(Bytes::from(crate::DOG_BIKE_CAR_BYTES), None, None)?;
@@ -398,6 +546,7 @@ fn initialize_onnx(
 ) -> Result<(DeviceType, String, Session, EndpointProvider), anyhow::Error> {
     let mut providers = Vec::new();
     let mut device_type = DeviceType::CPU;
+
     let (num_intra_threads, num_inter_threads) = if onnx_config.force_cpu {
         let num_intra_threads = onnx_config.intra_threads.min(num_cpus::get_physical() - 1);
         let num_inter_threads = onnx_config.inter_threads.min(num_cpus::get_physical() - 1);
@@ -413,6 +562,7 @@ fn initialize_onnx(
                 .build()
                 .error_on_failure(),
         );
+
         device_type = DeviceType::GPU;
         (1, 1) // For GPU we just hardcode to 1 thread
     } else {
@@ -421,6 +571,7 @@ fn initialize_onnx(
         warn!("DirectML not available, falling back to CPU for inference with {} intra and {} inter threads", num_intra_threads, num_inter_threads);
         (onnx_config.intra_threads, onnx_config.inter_threads)
     };
+
     let (model_bytes, model_name) = if let Some(model) = onnx_config.model.clone() {
         let model_str = model
             .to_str()
@@ -433,28 +584,35 @@ fn initialize_onnx(
             .parent()
             .ok_or_else(|| anyhow::anyhow!("Failed to get parent directory of executable path"))?
             .join(crate::SMALL_RT_DETR_V2_MODEL_FILE_NAME);
+
         let Ok(model_bytes) = std::fs::read(&model_path) else {
             error!("Failed to read model file: {:?} ensure you either specify a model or that {} is in the same directory as binary", model_path, crate::SMALL_RT_DETR_V2_MODEL_FILE_NAME.to_string());
+
             bail!("Failed to read model file");
         };
+
         (
             model_bytes,
             crate::SMALL_RT_DETR_V2_MODEL_FILE_NAME.to_string(),
         )
     };
+
     info!(
         "Initializing detector with model: {:?} and inference running on {}",
         model_name, device_type,
     );
+
     let session = Session::builder()?
         .with_execution_providers(providers)?
         .with_intra_threads(num_intra_threads)?
         .with_inter_threads(num_inter_threads)?
         .commit_from_memory(model_bytes.as_slice())?;
+
     let endpoint_provider = match device_type {
         DeviceType::GPU => EndpointProvider::DirectML,
         DeviceType::CPU => EndpointProvider::CPU,
     };
+
     Ok((device_type, model_name, session, endpoint_provider))
 }
 
--- src/download_models.rs
+++ src/download_models.rs
@@ -1,9 +1,10 @@
+use hf_hub::api::tokio::Api;
 use std::path::PathBuf;
 use tokio::fs;
 use tracing::info;
 
 const RT_DETR2_MODELS: &[(&str, &[&str])] = &[(
-    // TODO: Update the model repository name to rt-detrv2-onnx
+    // Messed up the repo name, should be rt-detr2-onnx
     "xnorpx/rt-detr2-onnx",
     &[
         "rt-detrv2-s.onnx",
@@ -14,7 +15,28 @@ const RT_DETR2_MODELS: &[(&str, &[&str])] = &[(
     ],
 )];
 
-pub fn download_models(model_path: PathBuf) -> anyhow::Result<()> {
+const CUSTOM_YOLO5_MODELS: &[(&str, &[&str])] = &[(
+    // Messed up the repo name, should be rt-detr2-onnx
+    "xnorpx/blue-onyx-yolo5",
+    &[
+        "delivery.onnx",
+        "delivery.yaml",
+        "IPcam-animal.onnx",
+        "IPcam-animal.yaml",
+        "ipcam-bird.onnx",
+        "ipcam-bird.yaml",
+        "IPcam-combined.onnx",
+        "IPcam-combined.yaml",
+        "IPcam-dark.onnx",
+        "IPcam-dark.yaml",
+        "IPcam-general.onnx",
+        "IPcam-general.yaml",
+        "package.onnx",
+        "package.yaml",
+    ],
+)];
+
+pub fn download_models(model_path: PathBuf, download_custom_yolo5: bool) -> anyhow::Result<()> {
     let rt = tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()
@@ -23,22 +45,40 @@ pub fn download_models(model_path: PathBuf) -> anyhow::Result<()> {
         if !model_path.exists() {
             fs::create_dir_all(model_path.clone()).await?;
         }
-        let mut downloaded_models = Vec::new();
-        for (api_name, models) in RT_DETR2_MODELS.iter() {
-            let api = hf_hub::api::tokio::Api::new()?;
-            let api = api.model(api_name.to_string());
-            for filename in models.iter() {
-                let cached_file_path = api.get(filename).await?;
-                tokio::fs::copy(cached_file_path, model_path.join(filename)).await?;
-                info!(
-                    "Copied {} to {}",
-                    filename,
-                    model_path.join(filename).display()
-                );
-                downloaded_models.push(filename);
-            }
+        let api = Api::new()?;
+        let mut downloaded_models: Vec<String> = Vec::new();
+        if download_custom_yolo5 {
+            download(api, CUSTOM_YOLO5_MODELS, &mut downloaded_models, model_path)
+                .await
+                .unwrap();
+        } else {
+            download(api, RT_DETR2_MODELS, &mut downloaded_models, model_path)
+                .await
+                .unwrap();
         }
         info!("Succesfully downloaded models: {:?}", downloaded_models);
         Ok(())
     })
+}
+
+async fn download(
+    api: Api,
+    models: &[(&str, &[&str])],
+    downloaded_models: &mut Vec<String>,
+    model_path: PathBuf,
+) -> anyhow::Result<()> {
+    for (api_name, models) in models.iter() {
+        let api = api.model(api_name.to_string());
+        for filename in models.iter() {
+            let cached_file_path = api.get(filename).await?;
+            tokio::fs::copy(cached_file_path, model_path.join(filename)).await?;
+            info!(
+                "Copied {} to {}",
+                filename,
+                model_path.join(filename).display()
+            );
+            downloaded_models.push(filename.to_string());
+        }
+    }
+    Ok(())
 }
